e14f2fc71c722681b4ffaf28a9847658d6276cef
fputra-source <frian.putra@gmail.com>
2026-05-17 00:22:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,45 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
+const CACHE_NAME = 'kasir-amp-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', function(e) {
-  return self.clients.claim();
+// Fetch dari Cache atau Network
+self.addEventListener('fetch', event => {
+  // Hanya intercept request GET
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return dari cache jika ada, jika tidak fetch dari network
+        return response || fetch(event.request);
+      })
+  );
 });
 
-// Fetch listener ini WAJIB ada agar Chrome menganggap PWA ini sah
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request).catch(function() {
-      return new Response('Offline');
-  }));
+// Update Cache
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
